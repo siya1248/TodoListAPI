9c36bec4d1fb1b8cef388be64af7ec494e6892e7
siya1248 <71254522+siya1248@users.noreply.github.com>
2024-02-15 01:07:08 +0000

    created ToDoItem class. added references

---
+++ TodoList.Models/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoList.Models;
+public class ToDoItem
+{
+    public int Id {get; set;}
+    public DateTime DueDate {get; set;}
+    public DateTime? CompletedDate {get; set;}
+    public string Description {get; set;}
+
+}
+
